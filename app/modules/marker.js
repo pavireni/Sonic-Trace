0649cab0a5ac4df286e9389c61abdc91aba6ac13
Rick Waldron <waldron.rick@gmail.com>
2013-01-04 13:48:09 -0500

    Marker Icon rendering
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- app/modules/marker.js
+++ app/modules/marker.js
@@ -3,12 +3,11 @@ define([
 
 ], function( App ) {
 
-
     var Marker = App.module();
 
     Marker.Model = Backbone.Model.extend({
         defaults: {
-            isRendered: false
+            icon: null
         }
     });
 
@@ -22,11 +21,50 @@ define([
         parse: function( obj ) {
             var ids = _.pluck( this.models, "id" );
 
-            // console.log( "IDS", _.pluck( this.models, "cid" ) );
+            // Enforce new-items-only mutations. This prevents
+            // previously existing marker items from being treated
+            // as newly added merker items on each fetch response.
             return ids.length === 0 ?
                 obj.items : obj.items.filter(function( item ) {
                     return ids.indexOf( item.id ) === -1;
                 });
+        },
+
+        render: function() {
+            var surface = Sonic.surfaces.la;
+
+            // TODO: From here, initialize any defaults that will be
+            // needed for ALL newly rendered markers
+            //
+            // L.Icon:
+            // See: http://leafletjs.com/examples/custom-icons.html
+            //
+            //
+
+            this.forEach(function( mark ) {
+                if ( mark.get("icon") === null ) {
+
+                    // TODO: From here, initialize any defaults that
+                    // will be needed for each individual marker
+                    // ie. based on specific marker data
+
+                    // Generate a Leaflet marker, which is added to the
+                    // main map surface.
+                    mark.set( "icon",
+                        new L.Marker([
+                            mark.get("media_geo_latitude"),
+                            mark.get("media_geo_longitude")
+                        ]).addTo( surface )
+                    );
+                }
+            });
+
+
+            // TODO: Determine a meaningful return value.
+            //  - Number of newly rendered markers?
+            //  - Boolean success flag?
+            //  - Marker.Collection instance?
+            //
         }
     });
 
@@ -40,23 +78,11 @@ define([
         var marks;
 
         if ( type === "reset" || type === "add" ) {
-            // Filter Marker Items for markers that aren't
-            // yet rendered onto the map surface.
-            marks = Marker.Items.filter(function( item ) {
-                return !item.get("isRendered");
-            });
-
-            console.log( "Marker.Items: Unrendered", marks );
-
-            // 1. Render the markers
-            //
-            // 2. Update isRendered = true
-            //
-            //
+            this.render();
         }
     });
 
-    // Fetch "bootstrap" Markers
+    // Fetch "bootstrap" Marker data
     Marker.Items.fetch();
 
     return Marker;
