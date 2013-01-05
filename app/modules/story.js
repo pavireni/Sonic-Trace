401a2a69d965c6b98428b831c414b3c93903baff
Rick Waldron <waldron.rick@gmail.com>
2013-01-05 15:19:28 -0500

    Initializing, requesting and validating Story models
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- app/modules/story.js
+++ app/modules/story.js
@@ -7,16 +7,47 @@ define([
 
     Story.Model = Backbone.Model.extend({
         defaults: {
+            isAvailable: false
         },
+        url: function() {
+            return "http://alpha.zeega.org/api/items/" + this.id;
+        },
+        parse: function( obj ) {
+            var data = obj.items[ 0 ].text;
 
+            // API requests are coming up empty handed for the
+            // "text" property now??
+            if ( !data ) {
+                console.warn(
+                    "Zeega API failed to respond with project data. Requested from: ", this.url()
+                );
+            }
+
+
+            return data;
+        },
         initialize: function() {
-            // Ensure no duplicates?
+            // Push all new Story.Model instances into
+            // the Story.Items collection
+            Story.Items.add( this );
+
+            this.set( "isAvailable", true );
         }
     });
 
     Story.Collection = Backbone.Collection.extend({
-
+        model: Story.Model
     });
+
+    Story.Items = new Story.Collection();
+
+    Story.Items.from = function( marks ) {
+        if ( marks.length ) {
+            marks.forEach(function( story ) {
+                ( new Story.Model(story) ).fetch();
+            });
+        }
+    };
 
     return Story;
 });
