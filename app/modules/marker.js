7b7974ea74b29837541dea9b6045b2eef312c91b
James Burns <jameburn@gmail.com>
2013-02-21 19:44:41 -0500

    update api url

--- app/modules/marker.js
+++ app/modules/marker.js
@@ -20,7 +20,7 @@ define([
 
         cache: true,
 
-        url: "http://alpha.zeega.org/api/items/73916/items",
+        url: "http://zeega.com/api/items/search?sort=date-desc&sort=date-desc&collection=73916&page=1&fields=id,media_geo_longitude,media_geo_latitude,tags,title",
 
         parse: function( obj ) {
             var ids = _.pluck( this.models, "id" );
